59ea357457a5a593a264c133db371b56145768e3
damyvegaleiva <damyvegaleiva@gmail.com>
2023-11-17 18:16:12 -0800

    Tic-Tac-Memo v2.0 (minor fix).

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -14,7 +14,7 @@ const Board: React.FC<BoardProps> = ({
   isTied,
 }) => {
   return (
-    <div className=" w-[360px] md:w-[750px] lg:w-[800px] gap-1.5 md:gap-y-4 md:gap-x-3 flex flex-row flex-wrap justify-around items-center m-auto ">
+    <article className=" w-[360px] md:w-[750px] lg:w-[800px] gap-1.5 md:gap-y-4 md:gap-x-3 flex flex-row flex-wrap justify-around items-center m-auto ">
       {board.map((_, index) => (
         <Square
           key={index}
@@ -25,7 +25,7 @@ const Board: React.FC<BoardProps> = ({
           isTied={isTied}
         />
       ))}
-    </div>
+    </article>
   );
 };
 
--- src/components/WinnerModal.tsx
+++ src/components/WinnerModal.tsx
@@ -12,7 +12,7 @@ const WinnerModal: React.FC<WinnerModalProps> = ({
   isTied,
 }) => {
   return (
-    <div className="absolute z-20 flex justify-center w-screen h-screen m-auto md:bottom-0 md:left-0 md:right-0 md:top-0 bg-black-clear">
+    <section className="absolute z-20 flex justify-center w-screen h-screen m-auto md:bottom-0 md:left-0 md:right-0 md:top-0 bg-black-clear">
       {/* --------------------------MODAL-------------------------- */}
       <div className=" mb-auto mt-[180px] md:m-auto rounded-lg bg-white/5 border-2 backdrop-blur-md w-[300px] md:w-[700px] flex-col flex border-[#003865]">
         <h2 className="text-center text-[2rem] md:text-[3rem] text-white mb-20 winner-title">
@@ -21,7 +21,7 @@ const WinnerModal: React.FC<WinnerModalProps> = ({
 
         <ResetButton handleReset={handleReset} />
       </div>
-    </div>
+    </section>
   );
 };
 
