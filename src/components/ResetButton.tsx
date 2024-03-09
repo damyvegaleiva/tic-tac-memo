0b5d1f81c50ac9ea952399e5b5ae446395054773
damyvegaleiva <damyvegaleiva@gmail.com>
2024-03-09 02:45:31 -0600

    Reset button animation added. Minor fixes.

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -5,7 +5,7 @@ type ResetButtonProps = {
 const ResetButton: React.FC<ResetButtonProps> = ({ handleReset }) => {
   return (
     <button
-      className="w-[125px] h-[42px] md:w-[150px] mb-4 md:h-[48px] border-2 rounded-md border-white m-auto transition duration-1000 text-white hover:bg-white hover:text-black"
+      className="w-[125px] animate-bounce text-black h-[40px] md:w-[150px] mb-4 md:h-[48px] border-2 rounded-md border-black m-auto transition duration-1000 text- bg-[#19c0f5] font-bold"
       onClick={handleReset}
     >
       RESET GAME
--- src/components/WinnerModal.tsx
+++ src/components/WinnerModal.tsx
@@ -12,10 +12,10 @@ const WinnerModal: React.FC<WinnerModalProps> = ({
   isTied,
 }) => {
   return (
-    <section className="absolute z-20 flex justify-center w-screen h-screen m-auto md:bottom-0 md:left-0 md:right-0 md:top-0 bg-black-clear">
+    <section className="absolute flex justify-center w-full h-full m-auto bg-black-clear">
       {/* --------------------------MODAL-------------------------- */}
-      <div className=" mb-auto mt-[180px] md:m-auto rounded-lg bg-white/5 border-2 backdrop-blur-md w-[300px] md:w-[700px] flex-col flex border-[#003865]">
-        <h2 className="text-center text-[2rem] md:text-[3rem] text-white mb-20 winner-title">
+      <div className=" m-auto rounded-lg bg-white/5 border-2 backdrop-blur-md w-[300px] md:w-[600px] flex-col flex border-[#003865]">
+        <h2 className="text-center text-[1.75rem] md:text-[3rem] text-white mb-20 winner-title">
           {isTied ? "Tied" : `Winner Player ${winner}`}
         </h2>
 
